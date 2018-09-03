4d4080fbeb8bfdc2ff0b8ccdf6bdcb71b857a469
Mariane <marianerobl@protonmail.com>
2018-09-03 12:04:31 -0300

    downgrade elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Extract.MixProject do
     [
       app: :extract,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: ">= 1.4.0",
       elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
